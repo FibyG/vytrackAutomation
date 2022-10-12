7eb7a946ce56a7d4fa7ff4f6848caebc19c08ebd
Milos <milostrpe@gmail.com>
2022-10-12 18:06:33 -0400

    Test Case 2 Verify Main Modules for Drivers

---
+++ src/test/java/com/vytrack/tests/US1_Modules_Verification/VerifyMainModulesTruckDriverTC2.java
@@ -0,0 +1,58 @@
+package com.vytrack.tests.US1_Modules_Verification;
+
+import com.vytrack.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class VerifyMainModulesTruckDriverTC2 {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = WebDriverFactory.getDriver("Chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testCase1() {
+        driver.get("https://qa2.vytrack.com/user/login");
+        // Step 1 - Log in with correct credentials.
+        WebElement usernameInput = driver.findElement(By.xpath("//input[@id='prependedInput']"));
+        usernameInput.sendKeys("user160");
+        WebElement passwordInput = driver.findElement(By.xpath("//input[@id='prependedInput2']"));
+        passwordInput.sendKeys("UserUser123");
+        WebElement loginButton = driver.findElement(By.xpath("//button[@type='submit']"));
+        loginButton.click();
+
+        // Step 2 - Verify drivers should view 4 modules names:
+        //1) Fleet:
+        WebElement verifyFleets = driver.findElement(By.xpath("(//span[@class='title title-level-1'])[1]"));
+        Assert.assertTrue(verifyFleets.isDisplayed());
+
+        //2) Customers:
+        WebElement verifyCustomer = driver.findElement(By.xpath("(//span[@class='title title-level-1'])[2]"));
+        Assert.assertTrue(verifyCustomer.isDisplayed());
+
+        //3) Activities:
+        WebElement verifyActivities = driver.findElement(By.xpath("(//span[@class='title title-level-1'])[3]"));
+        Assert.assertTrue(verifyActivities.isDisplayed());
+
+        //4) System:
+        WebElement verifySystem = driver.findElement(By.xpath("(//span[@class='title title-level-1'])[4]"));
+        Assert.assertTrue(verifySystem.isDisplayed());
+    }
+
+    @AfterMethod
+    public void TearDownMethod() {
+        driver.quit();
+    }
+    
+}
