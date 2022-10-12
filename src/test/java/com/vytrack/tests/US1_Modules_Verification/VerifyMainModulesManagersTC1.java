bc53b0304b2c6e6ad0e1814b3e9099ee486f8c6f
Milos <milostrpe@gmail.com>
2022-10-12 18:08:26 -0400

    Test Case 1 Verify Main Modules for Managers file renamed

--- src/test/java/com/vytrack/tests/US1_Modules_Verification/VerifyMainModulesManagersTC1.java
+++ src/test/java/com/vytrack/tests/US1_Modules_Verification/VerifyMainModulesManagersTC1.java
@@ -11,7 +11,7 @@ import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
-public class VerifyMainModulesStoreManagerTC1 {
+public class VerifyMainModulesManagersTC1 {
     WebDriver driver;
 
     @BeforeMethod
