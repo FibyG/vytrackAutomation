d4dd8f8d5cb9de28b2968c25ecd4db8a34a8f511
Milos <milostrpe@gmail.com>
2022-10-17 19:40:29 -0400

    TC2 updated

--- src/test/java/com/vytrack/tests/US1_Modules_Verification/VerifyMainModulesTruckDriverTC2.java
+++ src/test/java/com/vytrack/tests/US1_Modules_Verification/VerifyMainModulesTruckDriverTC2.java
@@ -1,58 +1,30 @@
 package com.vytrack.tests.US1_Modules_Verification;
 
-import com.vytrack.utilities.WebDriverFactory;
+import com.vytrack.tests.base.TestBase;
+import com.vytrack.utilities.ConfigurationReader;
+import com.vytrack.utilities.VYTrack_Login;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import java.util.ArrayList;
+import java.util.List;
 
-import java.util.concurrent.TimeUnit;
 
-public class VerifyMainModulesTruckDriverTC2 {
-    WebDriver driver;
+public class VerifyMainModulesTruckDriverTC2 extends TestBase {
 
-    @BeforeMethod
-    public void setUp() {
-        driver = WebDriverFactory.getDriver("Chrome");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    }
 
     @Test
-    public void testCase1() {
-        driver.get("https://qa2.vytrack.com/user/login");
+    public void test1() {
+        driver.get(ConfigurationReader.getProperty("env"));
         // Step 1 - Log in with correct credentials.
-        WebElement usernameInput = driver.findElement(By.xpath("//input[@id='prependedInput']"));
-        usernameInput.sendKeys("user160");
-        WebElement passwordInput = driver.findElement(By.xpath("//input[@id='prependedInput2']"));
-        passwordInput.sendKeys("UserUser123");
-        WebElement loginButton = driver.findElement(By.xpath("//button[@type='submit']"));
-        loginButton.click();
+        VYTrack_Login.vy_login(driver, ConfigurationReader.getProperty("username.driver1"), ConfigurationReader.getProperty("password"));
 
         // Step 2 - Verify drivers should view 4 modules names:
-        //1) Fleet:
-        WebElement verifyFleets = driver.findElement(By.xpath("(//span[@class='title title-level-1'])[1]"));
-        Assert.assertTrue(verifyFleets.isDisplayed(), "Verify truck driver can see modules FAILED!");
 
-        //2) Customers:
-        WebElement verifyCustomer = driver.findElement(By.xpath("(//span[@class='title title-level-1'])[2]"));
-        Assert.assertTrue(verifyCustomer.isDisplayed(), "Verify truck driver can see modules FAILED!");
+        List<WebElement> FourModules = new ArrayList<>(driver.findElements(By.xpath("//ul[@class='nav-multilevel main-menu']/li")));
+        for (WebElement each : FourModules) {
+            System.out.println(each.getText());
+        }
 
-        //3) Activities:
-        WebElement verifyActivities = driver.findElement(By.xpath("(//span[@class='title title-level-1'])[3]"));
-        Assert.assertTrue(verifyActivities.isDisplayed(), "Verify truck driver can see modules FAILED!");
-
-        //4) System:
-        WebElement verifySystem = driver.findElement(By.xpath("(//span[@class='title title-level-1'])[4]"));
-        Assert.assertTrue(verifySystem.isDisplayed(), "Verify truck driver can see modules FAILED!");
     }
-
-    @AfterMethod
-    public void TearDownMethod() {
-        driver.quit();
-    }
-
 }
