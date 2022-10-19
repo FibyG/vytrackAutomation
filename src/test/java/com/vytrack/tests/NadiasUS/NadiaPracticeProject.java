6e48f40dc8387b9a5780fd0b31b97254f00cbdab
Nadezhda Von Holt <nadezhdavonholt@gmail.com>
2022-10-18 21:05:12 -0400

    added///

--- src/test/java/com/vytrack/tests/NadiasUS/NadiaPracticeProject.java
+++ src/test/java/com/vytrack/tests/NadiasUS/NadiaPracticeProject.java
@@ -5,6 +5,7 @@ import com.vytrack.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -42,8 +43,11 @@ public class NadiaPracticeProject {
         //2.Fleet
         //WebElement FleetMenu = driver.findElement(By.xpath("//span[@class='title title-level-1'])[2]"));
         WebElement FleetMenu = driver.findElement(By.xpath("//ul[@class='nav-multilevel main-menu']/li[2]"));
-        FleetMenu.click();
-        Thread.sleep(3000);
+        //li[@class='dropdown dropdown-level-1'])[1]
+       Actions actions = new Actions(driver);
+       actions.moveToElement(FleetMenu).pause(4000).perform();
+        //FleetMenu.click();
+        //Thread.sleep(3000);
 
         //3.Vehicles.
         WebElement VehicleBtn = driver.findElement(By.xpath("//span[.='Vehicles']"));
@@ -68,5 +72,53 @@ public class NadiaPracticeProject {
         FirstCheckbox.click();
 
        // driver.quit();
+
     }
+
+        @Test
+        public void TC2()throws InterruptedException{
+            WebElement usernameBtn = driver.findElement(By.xpath("//input[@id='prependedInput']"));
+            usernameBtn.sendKeys("storemanager79");
+            WebElement passwordBtn = driver.findElement(By.id("prependedInput2"));
+            passwordBtn.sendKeys("UserUser123");
+            WebElement LoginBtn = driver.findElement(By.xpath("//button[@id='_submit']"));
+            LoginBtn.click();
+
+            Thread.sleep(3000);
+
+            //2.Fleet
+            //WebElement FleetMenu = driver.findElement(By.xpath("//span[@class='title title-level-1'])[2]"));
+            WebElement FleetMenu = driver.findElement(By.xpath("//ul[@class='nav-multilevel main-menu']/li[2]"));
+            //li[@class='dropdown dropdown-level-1'])[1]
+            Actions actions = new Actions(driver);
+            actions.moveToElement(FleetMenu).pause(4000).perform();
+            //FleetMenu.click();
+            //Thread.sleep(3000);
+
+            //3.Vehicles.
+            WebElement VehicleBtn = driver.findElement(By.xpath("//span[.='Vehicles']"));
+
+            //WebElement VehicleBtn = driver.findElement(By.xpath("//*[@id=\"main-menu\"]/ul/li[2]/div/div/ul/li[3]/a/span"));
+            VehicleBtn.click();
+            Thread.sleep(3000);
+
+            //4.Test all buttons are not selected
+            List<WebElement> AllChecboxes = driver.findElements(By.xpath("//input[@data-role='select-row-cell']"));
+            for(WebElement eachCheckbox : AllChecboxes){
+                if (eachCheckbox.isSelected()){
+                    System.out.println("All checkboxes are selected");
+                }else{
+                    System.out.println("All checkboxes are not selected");
+                }
+            }
+            Thread.sleep(3000);
+
+            //5.One of any cars is checked.
+            // WebElement AnyCarIsSelected = driver.findElement(By.xpath("//*[@id=\"grid-custom-entity-grid-1473493499\"]/div[2]/div[2]/div[2]/div/table/tbody/tr[2]/td[1]/input"));
+            WebElement AnyCarIsSelected = driver.findElement(By.xpath("//table[@class='grid table-hover table table-bordered table-condensed']//tbody//tr[1]//td[1]"));
+
+            AnyCarIsSelected.click();
+            //driver.quit();
+        }
+
 }
